219217b9ad4c768a06254d03207c0674dc6b3028
Carlos Hincapié <carmihinfer@gmail.com>
2014-10-01 18:00:41 -0500

    Haciendo el login

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,22 +1,23 @@
-<div class="container">
-	<div class="row">
-		<div class="col-xs-6 center col-xs-offset-3">
-			<div class="panel panel-success">
-				<div class="panel-heading">
-			   		<h3 class="panel-title">Login</h3>
-			  	</div>
-				<div class="panel-body">
-				   	<form action="" role="form" class="form form-horizontal">
-				   		<div class="form-group">
-				   			<div class="col-xs-10 col-xs-offset-1">
-				   				<input name="name" type="text" class="form-control required" placeholder="Nombre de usuario">
-				   			</div>
-							<div class="col-xs-10 col-xs-offset-1">
-				   				<input name="password" type="password" class="form-control required" placeholder="Contraseña">
-							<div>
-				   		</div>
-				   	</form>
-				</div>
+<div class="row">
+	<div class="col-xs-6 center col-xs-offset-3">
+		<div class="panel panel-success">
+			<div class="panel-heading">
+		   		<h3 class="panel-title">Ingresar</h3>
+		  	</div>
+			<div class="panel-body">
+			   	<form method="post" action="login" role="form" class="form form-horizontal">
+					<div class="col-xs-10 col-xs-offset-1">
+						<input name="name" type="text" class="form-control required" placeholder="Nombre de usuario">
+					</div>
+					<div class="col-xs-10 col-xs-offset-1">
+						<input name="password" type="password" class="form-control required" placeholder="Contraseña">
+					<div>
+					<div class="checkbox">
+						<label>
+			            <input type="checkbox" name="remember-me" value="remember-me"> Rcordarme?
+			          </label>
+			        </div>
+			   	</form>
 			</div>
 		</div>
 	</div>
