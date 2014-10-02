03fe71735591b77494b3348ed2e440404754971b
Oswaldo De la Ossa <oswaldo244@gmail.com>
2014-10-02 18:08:31 -0500

    Creando vista crear dispositivo

--- protected/views/dispositivo/create.php
+++ protected/views/dispositivo/create.php
@@ -1,30 +1,34 @@
 <h1 class="header-tittle">Dispositivos</h1>
-
-<div class="content">
-	<form class="form-inline" role="form">
-  <div class="form-group">
-    <label class="sr-only" for="exampleInputEmail2">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
-  </div>
-  <div class="form-group">
-    <div class="input-group">
-      <div class="input-group-addon">@</div>
-      <input class="form-control" type="email" placeholder="Enter email">
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="sr-only" for="exampleInputPassword2">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-  </div>
-  <div class="checkbox">
-    <label>
-      <input type="checkbox"> Remember me
-    </label>
-  </div>
-  <button type="submit" class="btn btn-default">Sign in</button>
-</form>
-<div class="content-side">
-	<h1>Tabla de disp</h1>
-</div>
-
+<div class="col-xs-10 col-xs-offset-1">
+	<form class="form form-horizontal" action="create" method="post" role="form"><br>
+		<div class="form-group col-xs-6">
+			<label for="dateAdq" class="col-xs-5 control-label">Fecha de adquisición:</label>
+			<div class="col-xs-7">
+				<input type="date" class="form-control" name="dateAdq" placeholder="dd/mm/aaaa">
+			</div>
+		</div>
+		<div class="form-group col-xs-6">
+			<label class="col-xs-5 control-label">Referencia:</label>
+			<div class="col-xs-7">
+				<input type="text" name="referencia" class="form-control" placeholder="Referencia">
+			</div>
+		</div>
+		<div class="form-group col-xs-6">
+			<label class="col-xs-5 control-label">IMEI o referencia:</label>
+			<div class="col-xs-7">
+				<input type="text" name="imei" class="form-control" placeholder="IMEI o referencia">
+			</div>
+		</div>
+		<div class="form-group col-xs-6">
+			<label class="col-xs-5 control-label">Estado:</label>
+			<div class="col-xs-7">
+				<input type="list" name="estado" class="form-control" placeholder="Seleccionar estado">
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-xs-offset-2 col-xs-10">
+				<button type="submit" class="btn btn-default">Sign in</button>
+			</div>
+		</div>
+	</form>
 </div>
